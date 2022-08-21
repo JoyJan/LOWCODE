1b30edd64c7eb13b98b3d30b38b2a58da412beb9
JackerDuuu <104127324+JoyJan@users.noreply.github.com>
2022-08-21 16:00:10 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/visual-maker/',
+  plugins: [vue()],
+  server: { // ← ← ← ← ← ←
+    host: '192.168.1.7' // ← 新增内容 ←
+    } ,// ← ← ← ← ← ←
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
+})
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/visual-maker/',
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
+})
